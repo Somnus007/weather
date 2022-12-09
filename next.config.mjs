47b0e3ded8ae4e2dab57664ef831cc30da147cdf
Allen Song <allen.song@h8tch.com>
2022-12-09 23:18:58 +0800

    Update config

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
+
+export default nextConfig;
